dcc22717fa459df4a0c3be5bd8dc1885c78ce03b
Ervine, John(e20042) <John.Ervine@cmegroup.com>
2015-08-05 09:34:29 +0100

    added vote job to main.clj file

--- src/govtrack_sync_clj/main.clj
+++ src/govtrack_sync_clj/main.clj
@@ -2,6 +2,7 @@
   (:gen-class)
   (:require [govtrack-sync-clj.legislators.legislators :as leg]
             [govtrack-sync-clj.bills.bills :as bills]
+            [govtrack-sync-clj.votes.votes :as votes]
             [clojure.tools.logging :as log]
             [clojure.edn :as edn]))
 
@@ -9,4 +10,5 @@
   (let [config (edn/read-string (slurp (first args)))]
     (log/info (str "Starting Sync Job against the following resources" config))
     (leg/persist-legislators config (:legislator-dir config) "congress" "legislator")
-    (bills/persist-bills config)))
+    (bills/persist-bills config)
+    (votes/persist-votes config)))
