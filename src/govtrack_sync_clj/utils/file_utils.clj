a89210bdc63c689ea68bc221798414791f27d298
Ervine, John(e20042) <John.Ervine@cmegroup.com>
2015-08-17 14:27:47 +0100

    Clean up, removed dead code in file_utils

--- src/govtrack_sync_clj/utils/file_utils.clj
+++ src/govtrack_sync_clj/utils/file_utils.clj
@@ -1,20 +1,6 @@
 (ns govtrack-sync-clj.utils.file-utils
   (:gen-class)
-  (:require [clojure.walk :as w]
-            [clojurewerkz.neocons.rest.nodes :as nn]
-            [clojurewerkz.neocons.rest.cypher :as cy]
-            [clojure.java.io :as io]))
-
-(defn get-node [connection id]
-  (if (nil? id)
-    nil
-    (nn/get connection id)))
-
-(defn retrieve-id [connection query]
-  (let [data (:data (cy/query connection query))]
-    (if (empty? data)
-      nil
-      (long (first (first data))))))
+  (:require [clojure.java.io :as io]))
 
 (defn walk [dirpath pattern]
   (doall (filter #(re-matches pattern (.getName %))
